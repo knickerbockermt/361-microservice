b64a7996e0a0f66da2ca3bfff1dcc369aa2bb25e
knickerbockermt <knickerbockermt@gmail.com>
2023-11-19 21:31:42 -0800

    finished microservice

---
+++ microservice.py
@@ -0,0 +1,25 @@
+import time
+import random
+
+while True:
+    file = open("microservice.txt", "r")
+    r = file.read()
+    file.close()
+
+    if "," in r:
+        r = r.split(",")
+
+        for i in range(len(r)):
+            r[i] = int(r[i])
+
+        mult, rng, modif = r
+
+        total = modif
+
+        for i in range(mult):
+            total += (random.randint(1, rng))
+
+        file = open("microservice.txt", "w")
+        file.write(str(total))
+        file.close()
+
